53cdda95ef10a1757454d677082db35a8ffacd41
jayrogers889csa <jayrogers889@gmail.com>
2014-03-12 14:17:38 -0500

    more basic config

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 MyKeyboards::Application.routes.draw do
+
+  resources :keyboards
+
+  get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
